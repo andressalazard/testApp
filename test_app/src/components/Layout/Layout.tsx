292dbeb44399d52eb8c2293bf1011736f5a66610
Andrés Salazar <andres.salazar.d.18@gmail.com>
2022-09-15 18:28:00 -0500

    first commit

---
+++ test_app/src/components/Layout/Layout.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Head from "next/head";
+import styles from "./Layout.module.css";
+
+function Layout({ children, title = "Test App" }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className={styles.header}>HEADER</header>
+
+      <main className={styles.main}>{children}</main>
+
+      <footer className={styles.footer}>
+        Developed by{" "}
+        <a href="https://github.com/andressalazard/">Andrés Salazar</a>
+      </footer>
+    </div>
+  );
+}
+
+export default Layout;
