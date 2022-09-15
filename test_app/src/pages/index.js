e599d1d1294bb349896457e3e519f17b6a3221ea
Andrés Salazar <andres.salazar.d.18@gmail.com>
2022-09-15 18:40:44 -0500

    fixed babel error

--- test_app/src/pages/index.js
+++ test_app/src/pages/index.js
@@ -1,8 +1,17 @@
-import Head from "next/head";
-import Image from "next/image";
 import Layout from "../components/Layout/Layout";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log(`Got ${posts.length} posts!`);
   return <Layout>HELLO WORLD</Layout>;
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+  return {
+    props: {
+      posts,
+    },
+  };
+};
